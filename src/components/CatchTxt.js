b673a3cba39f67e0ec17b5c50aff870e80e597ee
blancTo <git@blanc.to>
2023-11-10 10:08:22 +0900

    メインイメージをデバイスごとに参照する画像を変更するように調整

---
+++ src/components/CatchTxt.js
@@ -0,0 +1,104 @@
+import React, { useLayoutEffect, useState } from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+
+const CatchTxt = () => {
+  const [deviceType, setDeviceType] = useState('');
+
+  useLayoutEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+
+      if (width > 1024) {
+        setDeviceType('desktop');
+      } else if (width <= 1024 && width > 600) {
+        setDeviceType('tablet');
+      } else {
+        setDeviceType('mobile');
+      }
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const data = useStaticQuery(graphql`
+    query {
+      desktopImage: file(relativePath: { eq: "m_copy01.png" }) {
+        childImageSharp {
+          gatsbyImageData(width: 850, layout: CONSTRAINED)
+        }
+      }
+      tabletImage: file(relativePath: { eq: "m_copy01_tub.png" }) {
+        childImageSharp {
+          gatsbyImageData(width: 500, layout: CONSTRAINED)
+        }
+      }
+      mobileImage: file(relativePath: { eq: "m_copy01_smp.png" }) {
+        childImageSharp {
+          gatsbyImageData(width: 350, layout: CONSTRAINED)
+        }
+      }
+      pcEmbrem: file(relativePath: { eq: "emblem_01.png" }) {
+        childImageSharp {
+          gatsbyImageData(width: 640, layout: CONSTRAINED)
+        }
+      }
+      tubEmbrem: file(relativePath: { eq: "emblem_tub.png" }) {
+        childImageSharp {
+          gatsbyImageData(width: 370, layout: CONSTRAINED)
+        }
+      }
+      mobileEmbrem: file(relativePath: { eq: "emblem_01_smp.png" }) {
+        childImageSharp {
+          gatsbyImageData(width: 220, layout: CONSTRAINED)
+        }
+      }
+    }
+  `);
+
+  const desktopImage = getImage(data.desktopImage);
+  const tabletImage = getImage(data.tabletImage);
+  const mobileImage = getImage(data.mobileImage);
+  const pcEmbrem = getImage(data.pcEmbrem);
+  const tubEmbrem = getImage(data.tubEmbrem);
+  const mobileEmbrem = getImage(data.mobileEmbrem);
+
+  return (
+    <>
+      {deviceType === 'desktop' && (
+        <>
+          <GatsbyImage image={pcEmbrem} className='emblem' alt='' />
+
+          <h3 className='m_copy'>
+            <GatsbyImage image={desktopImage} alt='Desktop Image' />
+          </h3>
+        </>
+      )}
+      {deviceType === 'tablet' && (
+        <>
+          <GatsbyImage image={tubEmbrem} className='emblem' alt='' />
+
+          <h3 className='m_copy'>
+            <GatsbyImage image={tabletImage} alt='Tablet Image' />
+          </h3>
+        </>
+      )}
+      {deviceType === 'mobile' && (
+        <>
+          <GatsbyImage className='emblem emb_s' image={mobileEmbrem} alt='' />
+
+          <h3 className='m_copy'>
+            <GatsbyImage image={mobileImage} alt='Mobile Image' />
+          </h3>
+        </>
+      )}
+    </>
+  );
+};
+
+export default CatchTxt;
--- src/components/MainimageSub.js
+++ src/components/MainimageSub.js
@@ -1,14 +1,75 @@
-import React from "react";
+import React, { useLayoutEffect, useState } from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 const MainimageSub = (props) => {
+  const [deviceType, setDeviceType] = useState('');
+
+  useLayoutEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+
+      if (width > 1280) {
+        setDeviceType('desktop');
+      } else if (width <= 1280 && width > 768) {
+        setDeviceType('desktop_s');
+      } else if (width <= 768 && width > 600) {
+        setDeviceType('tablet');
+      } else {
+        setDeviceType('mobile');
+      }
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const data = useStaticQuery(graphql`
+    query {
+      desktopImage: file(relativePath: { eq: "mainimage_sub.jpg" }) {
+        childImageSharp {
+          gatsbyImageData(quality: 90, width: 350, layout: CONSTRAINED)
+        }
+      }
+      desktopsImage: file(relativePath: { eq: "mainimage_sub.jpg" }) {
+        childImageSharp {
+          gatsbyImageData(quality: 90, width: 250, layout: CONSTRAINED)
+        }
+      }
+      tabletImage: file(relativePath: { eq: "mainimage_sub.jpg" }) {
+        childImageSharp {
+          gatsbyImageData(width: 250, layout: CONSTRAINED)
+        }
+      }
+      mobileImage: file(relativePath: { eq: "mainimage_sub.jpg" }) {
+        childImageSharp {
+          gatsbyImageData(width: 250, layout: CONSTRAINED)
+        }
+      }
+    }
+  `);
+
+  const desktopImage = getImage(data.desktopImage);
+  const desktopsImage = getImage(data.desktopsImage);
+  const tabletImage = getImage(data.tabletImage);
+  const mobileImage = getImage(data.mobileImage);
+
   return (
     <>
-      <div id="mainimage_sub">
+      <div id='mainimage_sub'>
         {props.parentTitle ? (
           <h2>{props.parentTitle}</h2> // parentTitleが存在する場合
         ) : (
           <h2>{props.title}</h2> // parentTitleが存在しない場合
         )}
+        {deviceType === 'desktop' && <GatsbyImage className='mainimage_sub_bg' image={desktopImage} alt='Desktop Image' />}
+        {deviceType === 'desktop_s' && <GatsbyImage className='mainimage_sub_bg' image={desktopsImage} alt='Desktop Image' />}
+        {deviceType === 'tablet' && <GatsbyImage className='mainimage_sub_bg' image={tabletImage} alt='Tablet Image' />}
+        {deviceType === 'mobile' && <GatsbyImage className='mainimage_sub_bg' image={mobileImage} alt='Mobile Image' />}
       </div>
     </>
   );
