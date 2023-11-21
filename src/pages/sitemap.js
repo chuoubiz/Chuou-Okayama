c314e4f02b3e6dd9283b8a7901f998cc61135252
blancTo <git@blanc.to>
2023-11-22 07:44:03 +0900

    リンクエラー調整
    調査事例のよくある質問へid追加

--- src/pages/sitemap.js
+++ src/pages/sitemap.js
@@ -1,100 +1,100 @@
-import React from "react";
-import { Link } from "gatsby";
+import React from 'react';
+import { Link } from 'gatsby';
 
-import "modern-css-reset";
-import Layout from "../components/Layout";
-import Seo from "../components/Seo";
+import 'modern-css-reset';
+import Layout from '../components/Layout';
+import Seo from '../components/Seo';
 
 const sitemap = () => {
   return (
     <>
-      <Layout id="sitemap" title="サイトマップ">
-        <div className="sitemap_box">
+      <Layout id='sitemap' title='サイトマップ'>
+        <div className='sitemap_box'>
           <ul>
             <li>
-              <Link to="/">ホーム</Link>
+              <Link to='/'>ホーム</Link>
             </li>
             <li>
               調査項目
               <ul>
                 <li>
-                  <Link to="/unfaithful/">浮気・不倫・素行調査</Link>
+                  <Link to='/unfaithful/'>浮気・不倫・素行調査</Link>
                   <ul>
                     <li>
-                      <Link to="/unfaithful/type/">離婚の種類について</Link>
+                      <Link to='/unfaithful/type/'>離婚の種類について</Link>
                     </li>
                     <li>
-                      <Link to="/unfaithful/prove/">内容証明について</Link>
+                      <Link to='/unfaithful/prove/'>内容証明について</Link>
                     </li>
                   </ul>
                 </li>
                 <li>
-                  <Link to="/evidence/">裁判証拠収集</Link>
+                  <Link to='/evidence/'>裁判証拠収集</Link>
                 </li>
                 <li>
-                  <Link to="/survey/">結婚・身上調査</Link>
+                  <Link to='/survey/'>結婚・身上調査</Link>
                 </li>
                 <li>
-                  <Link to="/spying/">素行・内偵調査</Link>
+                  <Link to='/spying/'>素行・内偵調査</Link>
                 </li>
                 <li>
-                  <Link to="/whereabouts/">家出・所在調査</Link>
+                  <Link to='/whereabouts/'>家出・所在調査</Link>
                 </li>
                 <li>
-                  <Link to="/harassment/">嫌がらせ対策</Link>
+                  <Link to='/harassment/'>嫌がらせ対策</Link>
                 </li>
                 <li>
-                  <Link to="/wiretap/">盗聴・盗撮器発見</Link>
+                  <Link to='/wiretap/'>盗聴・盗撮器発見</Link>
                 </li>
                 <li>
-                  <Link to="/appraisal/">指紋・声紋・筆跡鑑定</Link>
+                  <Link to='/appraisal/'>指紋・声紋・筆跡鑑定</Link>
                 </li>
                 <li>
-                  <Link to="/corporation/">法人様向けサービス</Link>
+                  <Link to='/corporation/'>法人様向けサービス</Link>
                 </li>
               </ul>
             </li>
           </ul>
           <ul>
             <li>
-              <Link to="/flow/">ご利用の流れ</Link>
+              <Link to='/flow/'>ご利用の流れ</Link>
             </li>
             <li>
-              <Link to="/price/">料金表</Link>
+              <Link to='/price/'>料金表</Link>
             </li>
             <li>
-              <Link to="/case/">調査事例のご紹介</Link>
+              <Link to='/case/'>調査事例のご紹介</Link>
               <ul>
                 <li>
-                  <Link to="/case#faq/">よくある質問</Link>
+                  <Link to='/case/#faq'>よくある質問</Link>
                 </li>
               </ul>
             </li>
             <li>
-              <Link to="/company/">会社概要</Link>
+              <Link to='/company/'>会社概要</Link>
               <ul>
                 <li>
-                  <Link to="/company#office/">事業所一覧</Link>
+                  <Link to='/company/#office'>事業所一覧</Link>
                 </li>
                 <li>
-                  <Link to="/company#access/">アクセス</Link>
+                  <Link to='/company/#access'>アクセス</Link>
                 </li>
               </ul>
             </li>
             <li>
-              <Link to="/toiawase/">お問合せ</Link>
+              <Link to='/toiawase/'>お問合せ</Link>
             </li>
             <li>
-              <Link to="/recruit/">採用情報</Link>
+              <Link to='/recruit/'>採用情報</Link>
             </li>
             <li>
-              <Link to="/blog/">探偵ブログ</Link>
+              <Link to='/category/blog/'>探偵ブログ</Link>
             </li>
             <li>
-              <Link to="/sitemap/">サイトマップ</Link>
+              <Link to='/sitemap/'>サイトマップ</Link>
             </li>
             <li>
-              <Link to="/privacy-policy/">プライバシーポリシー</Link>
+              <Link to='/privacy/'>プライバシーポリシー</Link>
             </li>
           </ul>
         </div>
@@ -105,7 +105,7 @@ const sitemap = () => {
 
 export const Head = () => (
   <>
-    <body className="subpage" />
+    <body className='subpage' />
     <Seo />
   </>
 );
