706300f8e2e29f0f4bd5836f84c78053a1d4a7d7
blancTo <git@blanc.to>
2023-11-22 07:43:14 +0900

    調査写真のサンプル部分表示調整
    スマホ時非表示から表示するように、img→StaticImageに変更

--- src/components/TopSlide.js
+++ src/components/TopSlide.js
@@ -1,99 +1,68 @@
-import React from 'react'
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.min.css';
 import SwiperCore, { Pagination, Navigation } from 'swiper';
+import { StaticImage } from 'gatsby-plugin-image';
 
 SwiperCore.use([Pagination]);
 
 const TopSlide = () => {
   return (
     <>
-    <Swiper    
-       modules={[Pagination, Navigation]}
-       pagination={{ clickable: true }}
-       navigation
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-      breakpoints={{
-        768: {
-          slidesPerView: 1,
-        },
-        1024: {
+      <Swiper
+        modules={[Pagination, Navigation]}
+        pagination={{ clickable: true }}
+        navigation
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+        breakpoints={{
+          768: {
+            slidesPerView: 1,
+          },
+          1024: {
             slidesPerView: 6,
-          }
-      }}
-      slidesPerView={1}
-      >
+          },
+        }}
+        slidesPerView={1}>
         <SwiperSlide>
-          <div className="item">
-            <img
-              src="images/survey_img03.jpg"
-              width={640}
-              height={480}
-              alt=""
-            />
+          <div className='item'>
+            <StaticImage src='../images/survey_img03.jpg' width={640} height={480} alt='' />
             <p>〇〇付近で合流、2人は徒歩にて移動。</p>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-        <div className="item">
-            <img
-              src="images/survey_img04.jpg"
-              width={640}
-              height={480}
-              alt=""
-            />
+          <div className='item'>
+            <StaticImage src='../images/survey_img04.jpg' width={640} height={480} alt='' />
             <p>その後、19時09分、ラブホテル〇〇へと入るのを確認する。</p>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-        <div className="item">
-            <img
-              src="images/survey_img05.jpg"
-              width={640}
-              height={480}
-              alt=""
-            />
+          <div className='item'>
+            <StaticImage src='../images/survey_img05.jpg' width={640} height={480} alt='' />
             <p>21時11分、同2人は同ホテルより出るのを確認する。</p>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-        <div className="item">
-            <img
-              src="images/survey_img06.jpg"
-              width={640}
-              height={480}
-              alt=""
-            />
+          <div className='item'>
+            <StaticImage src='../images/survey_img06.jpg' width={640} height={480} alt='' />
             <p>一般のカメラで撮影した時。</p>
           </div>
-          </SwiperSlide>
-        <SwiperSlide className='item pc'>
-          <div className="item">
-            <img
-              src="images/survey_img07.jpg"
-              width={640}
-              height={480}
-              alt=""
-            />
+        </SwiperSlide>
+        <SwiperSlide className='item'>
+          <div className='item'>
+            <StaticImage src='../images/survey_img07.jpg' width={640} height={480} alt='' />
             <p>弊社の高感度カメラで撮影した時。</p>
           </div>
-          </SwiperSlide>
+        </SwiperSlide>
         <SwiperSlide>
-          <div className="item">
-            <img
-              src="images/survey_img08.jpg"
-              width={640}
-              height={480}
-              alt=""
-            />
+          <div className='item'>
+            <StaticImage src='../images/survey_img08.jpg' width={640} height={480} alt='' />
             <p>〇〇へ移動し同所にて別々に別れた為、相手の帰宅先を割り出す。</p>
           </div>
-          </SwiperSlide>
+        </SwiperSlide>
       </Swiper>
-      
     </>
-  )
-}
+  );
+};
 
-export default TopSlide
+export default TopSlide;
